5a5a20aafdcb6f777321d04e40c1bedb2248bad6
mariusmelas <melaas@gmail.com>
2020-02-16 16:38:43 +0100

    Updates

---
+++ precache-manifest.2d634602bf94c48efe5f2cc3dc3c77c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77bc05000683d0b83a8e82a0d2ff5ac2",
+    "url": "/pattern-from-image/index.html"
+  },
+  {
+    "revision": "438500ea69dc7ced1d91",
+    "url": "/pattern-from-image/static/css/main.6d1637d5.chunk.css"
+  },
+  {
+    "revision": "bb8330e3bde377542fe0",
+    "url": "/pattern-from-image/static/js/2.997a7e31.chunk.js"
+  },
+  {
+    "revision": "438500ea69dc7ced1d91",
+    "url": "/pattern-from-image/static/js/main.6ead8d31.chunk.js"
+  },
+  {
+    "revision": "345557e1c41a64cf582d",
+    "url": "/pattern-from-image/static/js/runtime-main.a00c0dde.js"
+  }
+]);
